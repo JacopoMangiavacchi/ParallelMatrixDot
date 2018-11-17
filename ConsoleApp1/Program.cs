c812d30d027359d4ea08ffc8e3a8495ebef7065b
Jacopo Mangiavacchi <jamangia@microsoft.com>
2018-11-16 22:03:08 -0800

    Adding Parallel recurrent calls

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Text;
 
 namespace ConsoleApp1
@@ -10,22 +10,24 @@ namespace ConsoleApp1
             Random random = new Random();
 
             //var m = new Matrix<int>(4, 2);
-            var m1 = new Matrix(4, 2);
+            //var m1 = new Matrix(4, 2);
+            var m1 = new Matrix(7, 5);
             m1.Randomize(() => random.Next(1, 9));
-            Console.WriteLine(m1.Description());
+            Console.WriteLine(m1.Description);
 
             Console.WriteLine("=======");
-            var m2 = new Matrix(2, 3);
+            //var m2 = new Matrix(2, 3);
+            var m2 = new Matrix(5, 6);
             m2.Randomize(() => random.Next(1, 9));
-            Console.WriteLine(m2.Description());
+            Console.WriteLine(m2.Description);
 
             Console.WriteLine("=======");
             var m3 = m1.Multiply(m2);
-            Console.WriteLine(m3.Description());
+            Console.WriteLine(m3.Description);
 
             Console.WriteLine("=======");
-            var m4 = m3.SubMatrix(1, 2, 1, 2);
-            Console.WriteLine(m4.Description());
+            var m4 = m3.Sub(1, 2, 1, 2);
+            Console.WriteLine(m4.Description);
         }
     }
 }
