5043bb3e4d2c69b691e71946810a1d4acaf50a81
Jacopo Mangiavacchi <jamangia@microsoft.com>
2018-11-18 10:38:28 -0800

    Added Generic support

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,29 +8,35 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Random random = new Random();
+            Func<int, int, int> multiplier = (a, b) => a*b;
+            Func<int, int, int> adder = (a, b) => a + b;
+            Func<int> randomizer = () => random.Next(1, 9);
 
-            //var m = new Matrix<int>(4, 2);
-            //var m1 = new Matrix(4, 2);
-            //var m2 = new Matrix(2, 3);
-            var m1 = new Matrix(47, 23);
-            var m2 = new Matrix(23, 32);
+            var m1 = new Matrix<int>(471, 236, multiplier, adder, 0, randomizer);
+            var m2 = new Matrix<int>(236, 862, multiplier, adder, 0, randomizer);
 
-            m1.Randomize(() => random.Next(1, 9));
-            m2.Randomize(() => random.Next(1, 9));
+            Matrix<int> r;
 
-            Console.WriteLine(m1.Description);
+            //Console.WriteLine(m1.Description);
+            //Console.WriteLine("=======");
+            //Console.WriteLine(m2.Description);
 
-            Console.WriteLine("=======");
-            Console.WriteLine(m2.Description);
+            //Console.WriteLine("=======");
+            r = m1.MultiplyIterative(m2);
+            //Console.WriteLine(r.Description);
 
-            Console.WriteLine("=======");
-            Console.WriteLine(m1.MultiplyIterative(m2).Description);
+            //Console.WriteLine("=======");
+            r = m1.MultiplyRecursiveBox(m2, 2, 2);
+            r = m1.MultiplyRecursiveBox(m2, 4, 4);
+            r = m1.MultiplyRecursiveBox(m2, 10, 10);
+            r = m1.MultiplyRecursiveBox(m2, 100, 100);
+            r = m1.MultiplyRecursiveBox(m2, 200, 200);
+            r = m1.MultiplyRecursiveBox(m2, 1000, 1000);
+            //Console.WriteLine(r.Description);
 
-            Console.WriteLine("=======");
-            Console.WriteLine(m1.MultiplyRecursiveBox(m2).Description);
-
-            Console.WriteLine("=======");
-            Console.WriteLine(m1.MultiplyRecursiveVector(m2).Description);
+            //Console.WriteLine("=======");
+            r = m1.MultiplyRecursiveVector(m2);
+            //Console.WriteLine(r.Description);
         }
     }
 }
